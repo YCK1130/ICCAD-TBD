9e16cf5f73194a74294dbce30832f99d51748c90
YCK1130 <ken1130yang@gmail.com>
2024-05-31 20:49:43 +0800

    test some input output files

---
+++ src/libs/utils.py
@@ -0,0 +1,81 @@
+import json
+from pathlib import Path
+
+
+def dict_append(d: dict, key: str, value: any) -> dict:
+    if key in d:
+        d[key].append(value)
+    else:
+        d[key] = [value]
+    return d
+
+
+def parse_lib(lib: dict) -> dict:
+    assert 'information' in lib and 'cells' in lib, 'Invalid lib format'
+    '''Parse the library to a more readable format
+    Input:
+        lib: dict
+    Output:
+        data: {
+            'info': dict,
+            'types': {
+                'cell_type': [cell_name]
+            },
+            'cells': [{
+                'cell_name',
+                'cell_type', 
+                ...
+            }]
+        }
+    '''
+    data = {}
+    data['info'] = lib['information']
+    data['types'] = {}
+    data['cells'] = {}
+    lib_cells = lib['cells']
+    for cell in lib_cells:
+        dict_append(data['types'], cell['cell_type'], cell['cell_name'])
+        data['cells'][cell['cell_name']] = cell
+    return data
+
+
+def parse_netlist(netlist_str: str) -> dict:
+    netlist = netlist_str.replace('\n', '').replace(
+        '\t', '').split(';')
+    netlist = [line.strip() for line in netlist if line.strip()]
+    head = netlist[0].replace('module', '\nmodule')
+    end = netlist[-1]
+    wires = []
+    inputs = []
+    outputs = []
+    gates = []
+    for line in netlist[1:-1]:
+        if 'wire' in line:
+            wires.append(line.split(' ')[1])
+        elif 'input' in line:
+            inputs.append(line.split(' ')[1])
+        elif 'output' in line:
+            outputs.append(line.split(' ')[1])
+        else:
+            gates.append(line.replace('    ', ' ').replace('  ', ' '))
+
+    cleaned_netlist = '{head};\n\tinput {inputs};\n\toutput {outputs};\n\twire {wires};\n\t{gates};\n{end}'.format(
+        head=head, wires=', '.join(wires),
+        inputs=', '.join(inputs), outputs=', '.join(outputs),
+        gates=';\n\t'.join(gates), end=end)
+    return cleaned_netlist
+
+
+class TmpDir:
+    def __init__(self, tmp_dir: Path):
+        self.tmp_dir = tmp_dir
+
+    def __enter__(self):
+        self.tmp_dir.mkdir(parents=True, exist_ok=True)
+        return self.tmp_dir
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type:
+            print(f"Error: {exc_val}")
+            print(f"Traceback: {exc_tb}")
+        self.tmp_dir.rmdir()
---
+++ src/main.py
@@ -0,0 +1,43 @@
+import json
+import subprocess
+import tqdm
+from libs.utils import parse_lib, parse_netlist
+import argparse
+from pathlib import Path
+
+
+def parse_args():
+    all_required = False
+    parser = argparse.ArgumentParser(
+        description='We aim to optimize a netlist with...')
+    parser.add_argument('-library', type=str, default='lib.json', required=all_required,
+                        help='Path to the libraries json file')
+    parser.add_argument('-netlist', type=str, default='design.v', required=all_required,
+                        help='Path to the netlist file')
+    parser.add_argument('-cost_function', type=str, default='cost_function_1', required=all_required,
+                        help='Cost function to optimize')
+    parser.add_argument('-output', type=str, default='design_optimized.v', required=all_required,
+                        help='Path to the output optimized netlist file')
+    parser.add_argument('--outdir', '-od', type=str, default='data/parsed/',
+                        help='Path to the output directory')
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    with open(args.library, 'r') as f:
+        lib = json.load(f)
+    data = parse_lib(lib)
+    if not Path(args.outdir).exists():
+        Path(args.outdir).mkdir(parents=True, exist_ok=True)
+    with open(f"{args.outdir}/test.json", 'w') as f:
+        json.dump(data, f, indent=4)
+    print("All libraries parsed successfully!")
+    print(f"Writing to {args.outdir}/test.json")
+
+    with open(args.netlist, 'r') as f:
+        netlist_str = f.read()
+    cleaned_netlist = parse_netlist(netlist_str)
+    with open(f"{args.outdir}/netlist_cleaned.v", 'w') as f:
+        f.write(cleaned_netlist)
